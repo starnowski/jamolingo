8e740bed270f85b6ef4b7f46448d441650bfeb6d
starnowski <33316705+starnowski@users.noreply.github.com>
2025-12-15 18:44:43 +0100

    #1 - Added mapping factory (not yet finished!)

---
+++ core/src/main/java/com/github/starnowski/jamolingo/context/ODataMongoMappingFactory.java
@@ -0,0 +1,11 @@
+package com.github.starnowski.jamolingo.context;
+
+import org.apache.olingo.commons.api.edm.EdmSchema;
+
+public class ODataMongoMappingFactory {
+
+  public ODataMongoMapping build(EdmSchema edmSchema) {
+    // TODO
+    return null;
+  }
+}
