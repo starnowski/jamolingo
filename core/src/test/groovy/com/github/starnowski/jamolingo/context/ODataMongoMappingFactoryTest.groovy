acb6297f85b64314e28b5c94606ef882452a34d2
starnowski <33316705+starnowski@users.noreply.github.com>
2025-12-16 20:58:27 +0100

    #1 - Added tests case

--- core/src/test/groovy/com/github/starnowski/jamolingo/context/ODataMongoMappingFactoryTest.groovy
+++ core/src/test/groovy/com/github/starnowski/jamolingo/context/ODataMongoMappingFactoryTest.groovy
@@ -1,6 +1,33 @@
 package com.github.starnowski.jamolingo.context
 
+import com.github.starnowski.jamolingo.AbstractSpecification
+import com.github.starnowski.jamolingo.select.OdataSelectToMongoProjectParser
+import org.apache.olingo.commons.api.edm.Edm
+import org.apache.olingo.server.api.OData
+import org.apache.olingo.server.api.uri.UriInfo
+import org.apache.olingo.server.core.uri.parser.Parser
+import org.bson.conversions.Bson
 import spock.lang.Specification
+import spock.lang.Unroll
 
-class ODataMongoMappingFactoryTest extends Specification {
+import java.util.stream.Collectors
+
+class ODataMongoMappingFactoryTest extends AbstractSpecification {
+
+    @Unroll
+    def "should return expected stage bson object"(){
+        given:
+            def tested = new ODataMongoMappingFactory()
+            Edm edm = loadEmdProvider(edmConfigFile)
+
+        when:
+            def result = tested.build(edm, schema)
+
+        then:
+            result == expectedODataMongoMapping
+
+        where:
+            edmConfigFile   | schema    || expectedODataMongoMapping
+            "edm/edm1.xml"  | "Demo"    || new ODataMongoMapping().withEntities(Map.of("Item", new EntityMapping().withProperties(Map.of("plainString", new PropertyMapping()))))
+    }
 }
