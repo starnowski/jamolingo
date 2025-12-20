690b01b25c5518096612188f61f4254a2f1bd48b
starnowski <33316705+starnowski@users.noreply.github.com>
2025-12-20 09:29:34 +0100

    #1 - Added TODO comment

--- core/src/test/groovy/com/github/starnowski/jamolingo/context/ODataMongoMappingFactoryTest.groovy
+++ core/src/test/groovy/com/github/starnowski/jamolingo/context/ODataMongoMappingFactoryTest.groovy
@@ -1,16 +1,9 @@
 package com.github.starnowski.jamolingo.context
 
 import com.github.starnowski.jamolingo.AbstractSpecification
-import com.github.starnowski.jamolingo.select.OdataSelectToMongoProjectParser
 import org.apache.olingo.commons.api.edm.Edm
-import org.apache.olingo.server.api.OData
-import org.apache.olingo.server.api.uri.UriInfo
-import org.apache.olingo.server.core.uri.parser.Parser
-import org.bson.conversions.Bson
-import spock.lang.Specification
 import spock.lang.Unroll
 
-import java.util.stream.Collectors
 
 class ODataMongoMappingFactoryTest extends AbstractSpecification {
 
@@ -30,5 +23,6 @@ class ODataMongoMappingFactoryTest extends AbstractSpecification {
             edmConfigFile   | schema    || expectedODataMongoMapping
             "edm/edm1.xml"  | "Demo"    || new ODataMongoMapping().withEntities(Map.of("Item", new EntityMapping().withCollection("Item").withProperties(Map.of("plainString", new PropertyMapping()))))
             "edm/edm2_with_nested_collections.xml"  | "Demo"    || new ODataMongoMapping().withEntities(Map.of("Item", new EntityMapping().withCollection("Item").withProperties(Map.of("plainString", new PropertyMapping(), "Name", new PropertyMapping(), "Addresses", new PropertyMapping().withProperties(Map.of("Street", new PropertyMapping(), "City", new PropertyMapping(), "ZipCode", new PropertyMapping())) ))))
+            //TODO Test case with circular types
     }
 }
