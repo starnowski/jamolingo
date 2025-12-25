b3176ffcc7d6b001a7372f620fd1faa1b35b7317
starnowski <33316705+starnowski@users.noreply.github.com>
2025-12-25 14:30:03 +0100

    #1 - Code refactoring

--- core/src/test/groovy/com/github/starnowski/jamolingo/context/EntityPropertiesMongoPathResolverTest.groovy
+++ core/src/test/groovy/com/github/starnowski/jamolingo/context/EntityPropertiesMongoPathResolverTest.groovy
@@ -8,12 +8,12 @@ import java.util.stream.Collectors
 class EntityPropertiesMongoPathResolverTest extends Specification {
 
     @Unroll
-    def "should return correct mongo properties mapping based on entity configuration #entityMapping"(){
+    def "should return correct mongo properties mapping based on entity configuration #entityMapping with only leafs"(){
         given:
             def tested = new EntityPropertiesMongoPathResolver()
 
         when:
-            def result = tested.resolve(entityMapping).entrySet().stream().collect(
+            def result = tested.resolve(entityMapping, new EntityPropertiesMongoPathResolver.EntityPropertiesMongoPathResolverContext.EntityPropertiesMongoPathResolverContextBuilder().withGenerateOnlyLeafs(true).build()).entrySet().stream().collect(
                     Collectors.toMap(
                             (entry) -> entry.getKey(), (entry) -> entry.getValue().getMongoPath()))
 
@@ -46,5 +46,46 @@ class EntityPropertiesMongoPathResolverTest extends Specification {
         // TODO Add mappings for object itself
         // TODO flatted
             //TODO Circular
+    }
+
+    @Unroll
+    def "should return correct mongo properties mapping based on entity configuration #entityMapping"(){
+        given:
+        def tested = new EntityPropertiesMongoPathResolver()
+
+        when:
+        def result = tested.resolve(entityMapping).entrySet().stream().collect(
+                Collectors.toMap(
+                        (entry) -> entry.getKey(), (entry) -> entry.getValue().getMongoPath()))
+
+        then:
+        result == expecteMongoPatsh
+
+        where:
+        entityMapping   ||  expecteMongoPatsh
+        new EntityMapping()
+                .withCollection("Item")
+                .withProperties(Map.of("plainString", new PropertyMapping()))
+                        || Map.of("plainString", "plainString")
+        new EntityMapping().withCollection("Item").withProperties(Map.of("plainString", new PropertyMapping(), "Name", new PropertyMapping(), "Addresses", new PropertyMapping().withProperties(Map.of("Street", new PropertyMapping(), "City", new PropertyMapping(), "ZipCode", new PropertyMapping())) ))
+                        || Map.of("plainString", "plainString", "Addresses.City", "Addresses.City", "Addresses.ZipCode","Addresses.ZipCode", "Addresses.Street","Addresses.Street", "Name", "Name")
+        // EDM flat property - Mongo nested object
+        new EntityMapping()
+                .withCollection("Item")
+                .withProperties(Map.of("plainString", new PropertyMapping().withRelativeTo("nestedObject")))
+                        || Map.of("plainString", "nestedObject.plainString")
+        // EDM nested object - Mongo flat property
+        new EntityMapping()
+                .withCollection("Item")
+                .withProperties(Map.of("nestedObject", new PropertyMapping().withProperties(Map.of("plainString", new PropertyMapping().withFlattenedLevelUp(1)) )))
+                        || Map.of("nestedObject.plainString", "plainString")
+        // EDM nested grand object - Mongo flat property
+//            new EntityMapping()
+//                    .withCollection("Item")
+//                    .withProperties(Map.of("nestedObject", new PropertyMapping().withProperties(Map.of("plainString", new PropertyMapping().withFlatterLevelUp(1)) )))
+//                    || Map.of("nestedObject.plainString", "plainString")
+        // TODO Add mappings for object itself
+        // TODO flatted
+        //TODO Circular
     }
 }
