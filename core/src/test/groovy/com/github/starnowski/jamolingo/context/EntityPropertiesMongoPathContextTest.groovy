55d285425fbf468c2b86f42fd0e0ad6b4e955d06
starnowski <33316705+starnowski@users.noreply.github.com>
2025-12-28 11:45:36 +0100

    #1 - Added tests case plan

---
+++ core/src/test/groovy/com/github/starnowski/jamolingo/context/EntityPropertiesMongoPathContextTest.groovy
@@ -0,0 +1,14 @@
+package com.github.starnowski.jamolingo.context
+
+import spock.lang.Specification
+
+class EntityPropertiesMongoPathContextTest extends Specification {
+
+    //TODO flatted properties (one-to-one) mapping
+    //TODO flatted properties (properties have different names and mongo paths levels) mapping
+    //TODO Complex types (one-to-one) mapping
+    //TODO Complex types (properties have different names and mongo paths levels) mapping
+    //TODO Circular reference (one-to-one) mapping with nested levels (no max level)
+    //TODO Circular reference (properties have different names and mongo paths levels) mapping with nested levels (no max level)
+    //TODO Circular reference with max level
+}
