9228cfcff0f827af0a65d942ae844c567c526339
starnowski <33316705+starnowski@users.noreply.github.com>
2025-12-29 23:06:01 +0100

    #1 - Added TODO comments

--- core/src/main/java/com/github/starnowski/jamolingo/context/EntityPropertiesMongoPathContext.java
+++ core/src/main/java/com/github/starnowski/jamolingo/context/EntityPropertiesMongoPathContext.java
@@ -15,7 +15,27 @@ public class EntityPropertiesMongoPathContext {
     MongoPathEntry entry = this.edmToMongoPath.get(edmPath);
     if (entry == null) {
       //TODO
-      return null;
+      String longestMatchingEDMPath = edmToMongoPath.keySet().stream().filter(edmPath::startsWith).max((s1, s2) -> {
+          if (s1.length() > s2.length()) {
+              return 1;
+          } else if (s1.length() < s2.length()) {
+              return -1;
+          }
+          return 0;
+      }).orElse(null);
+      if (longestMatchingEDMPath == null) {
+        //TODO exception
+      }
+      String baseMongoPath = this.edmToMongoPath.get(longestMatchingEDMPath).getMongoPath();
+      //TODO resolve type
+      //TODO get type mongoPath
+      // TODO in recurence
+      // TODO Remove longestMatchingEDMPath from edmPath -> tmpEDMPath
+      // TODO Concat type EDMPath and tmpEDMPath -> tmpEDMPath
+      // TODO Check if edmToMongoPath has tmpEDMPath
+      // TODO If yes then
+      // TODO end recurence
+      return baseMongoPath;
     } else {
       return entry.getMongoPath();
     }
