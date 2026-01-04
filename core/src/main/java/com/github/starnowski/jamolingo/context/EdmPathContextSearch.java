e12f1e31072607404985f49d3ad48c1a4fbfc05e
starnowski <33316705+starnowski@users.noreply.github.com>
2026-01-04 01:59:57 +0100

    #1 - Update interface

--- core/src/main/java/com/github/starnowski/jamolingo/context/EdmPathContextSearch.java
+++ core/src/main/java/com/github/starnowski/jamolingo/context/EdmPathContextSearch.java
@@ -2,5 +2,14 @@ package com.github.starnowski.jamolingo.context;
 
 public interface EdmPathContextSearch {
 
-  Integer getMongoPathMaxDepth();
+
+
+    Integer getMongoPathMaxDepth();
+
+
+
+    Integer getMaxCircularLimitPerEdmPath();
+
 }
+
+
