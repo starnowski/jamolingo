7bc62fce53475fb074473eca74f90d4393a9322e
starnowski <33316705+starnowski@users.noreply.github.com>
2026-01-06 11:07:26 +0100

    #1 - Test code refactoring

--- core/src/test/groovy/com/github/starnowski/jamolingo/select/OdataSelectToMongoProjectParserTest.groovy
+++ core/src/test/groovy/com/github/starnowski/jamolingo/select/OdataSelectToMongoProjectParserTest.groovy
@@ -35,14 +35,21 @@ class OdataSelectToMongoProjectParserTest extends AbstractSpecification {
             result.getStageObject() == expectedBson
 
         where:
-            bsonFile |  edmConfigFile   | selectFields
-            "select/stages/case1.json"       |  "edm/edm1.xml"  | ["plainString"]
-            "select/stages/case_wildcard_without_id.json"       |  "edm/edm1.xml"  | ["*"] // ExpandAsterisk = false
-            "select/stages/case2.json"       |  "edm/edm2_with_nested_collections.xml"  | ["plainString", "Name", "Addresses/Street", "Addresses/ZipCode"] // ExpandAsterisk = false
-            "select/stages/case2_with_whole_nested_object.json"       |  "edm/edm2_with_nested_collections.xml"  | ["plainString", "Name", "Addresses"] // ExpandAsterisk = false
-            "select/stages/case3_with_nested_complexType_circular_reference.json"       |  "edm/edm2_complextype_with_circular_reference.xml"  | ["plainString", "Name", "Addresses/Street", "Addresses/ZipCode", "Addresses/BackUpAddresses/ZipCode"] // ExpandAsterisk = false
-            "select/stages/case3_with_nested_complexType_circular_reference.json"       |  "edm/edm3_complextype_with_circular_reference_collection.xml"  | ["plainString", "Name", "Addresses/Street", "Addresses/ZipCode", "Addresses/BackUpAddresses/ZipCode"] // ExpandAsterisk = false
+            [bsonFile, edmConfigFile, selectFields] << oneToOneEdmPathsMappings()
     }
+
+    static oneToOneEdmPathsMappings() {
+        [
+                ["select/stages/case1.json"       ,  "edm/edm1.xml"  , ["plainString"]],
+                ["select/stages/case_wildcard_without_id.json"       ,  "edm/edm1.xml"  , ["*"]],// ExpandAsterisk = false
+                ["select/stages/case2.json"       ,  "edm/edm2_with_nested_collections.xml"  , ["plainString", "Name", "Addresses/Street", "Addresses/ZipCode"]], // ExpandAsterisk = false
+                ["select/stages/case2_with_whole_nested_object.json"       ,  "edm/edm2_with_nested_collections.xml"  , ["plainString", "Name", "Addresses"]], // ExpandAsterisk = false
+                ["select/stages/case3_with_nested_complexType_circular_reference.json"       ,  "edm/edm2_complextype_with_circular_reference.xml"  , ["plainString", "Name", "Addresses/Street", "Addresses/ZipCode", "Addresses/BackUpAddresses/ZipCode"]], // ExpandAsterisk = false
+                ["select/stages/case3_with_nested_complexType_circular_reference.json"       ,  "edm/edm3_complextype_with_circular_reference_collection.xml"  , ["plainString", "Name", "Addresses/Street", "Addresses/ZipCode", "Addresses/BackUpAddresses/ZipCode"]] // ExpandAsterisk = false
+        ]
+    }
+
+
 
     // TODO ExpandAsterisk = true (all fields defined in EDM)
 }
