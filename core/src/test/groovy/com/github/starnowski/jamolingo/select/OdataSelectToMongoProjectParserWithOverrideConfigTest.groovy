7b80aabd87dc6f50be0bb3c87eb4c5b325cfabce
starnowski <33316705+starnowski@users.noreply.github.com>
2026-01-07 21:01:50 +0100

    #1 - Added tests case with override configuration

---
+++ core/src/test/groovy/com/github/starnowski/jamolingo/select/OdataSelectToMongoProjectParserWithOverrideConfigTest.groovy
@@ -0,0 +1,78 @@
+package com.github.starnowski.jamolingo.select
+
+import com.github.starnowski.jamolingo.AbstractSpecification
+import com.github.starnowski.jamolingo.common.json.JSONOverrideHelper
+import com.github.starnowski.jamolingo.context.DefaultEdmMongoContextFacade
+import com.github.starnowski.jamolingo.context.EntityMapping
+import com.github.starnowski.jamolingo.context.EntityPropertiesMongoPathContextBuilder
+import com.github.starnowski.jamolingo.context.ODataMongoMappingFactory
+import org.apache.olingo.commons.api.edm.Edm
+import org.apache.olingo.server.api.OData
+import org.apache.olingo.server.api.uri.UriInfo
+import org.apache.olingo.server.core.uri.parser.Parser
+import org.bson.conversions.Bson
+import spock.lang.Unroll
+
+import java.util.stream.Collectors
+
+class OdataSelectToMongoProjectParserWithOverrideConfigTest extends AbstractSpecification {
+
+    @Unroll
+    def "should return expected stage bson object with default EdmMongoContextFacade with 1-to-1 edm to mongo mapping"() {
+        given:
+            Bson expectedBson = loadBsonFromFile(bsonFile)
+            Edm edm = loadEmdProvider(edmConfigFile)
+            ODataMongoMappingFactory factory = new ODataMongoMappingFactory()
+            def odataMapping = factory.build(edm.getSchema("Demo"))
+            def entityMapping = odataMapping.getEntities().get("Item")
+            EntityPropertiesMongoPathContextBuilder entityPropertiesMongoPathContextBuilder = new EntityPropertiesMongoPathContextBuilder()
+            def helper = new JSONOverrideHelper()
+            entityMapping = helper.applyChangesToJson(entityMapping, mergePayload as String, EntityMapping.class, JSONOverrideHelper.PatchType.MERGE)
+            def context = entityPropertiesMongoPathContextBuilder.build(entityMapping)
+
+
+            UriInfo uriInfo = new Parser(edm, OData.newInstance())
+                    .parseUri("Items",
+                            "\$select=" +
+                                    selectFields.stream().filter(Objects::nonNull)
+                                            .filter(s -> !s.trim().isEmpty())
+                                            .collect(Collectors.joining(","))
+                            , null, null)
+            OdataSelectToMongoProjectParser tested = new OdataSelectToMongoProjectParser()
+
+        when:
+            def result = tested.parse(uriInfo.getSelectOption(), new DefaultEdmMongoContextFacade(context, null))
+
+        then:
+            result.getStageObject() == expectedBson
+
+        where:
+            [bsonFile, edmConfigFile, mergePayload, selectFields] << oneToOneEdmPathsMappings()
+    }
+
+    static final String EDM_1_OVERRIDE_MERGE = """
+        {
+            "properties": {
+                "plainString": {
+                    "mongoName": "thisIsString"
+                }
+            }
+        }
+    """;
+
+        static oneToOneEdmPathsMappings() {
+        [
+                ["select/stages/case1_edm1_config_override.json",  "edm/edm1.xml", EDM_1_OVERRIDE_MERGE  , ["plainString"]]
+//                ,
+//                ["select/stages/case_wildcard_without_id.json"       ,  "edm/edm1.xml"  , ["*"]],// ExpandAsterisk = false
+//                ["select/stages/case2.json"       ,  "edm/edm2_with_nested_collections.xml"  , ["plainString", "Name", "Addresses/Street", "Addresses/ZipCode"]], // ExpandAsterisk = false
+//                ["select/stages/case2_with_whole_nested_object.json"       ,  "edm/edm2_with_nested_collections.xml"  , ["plainString", "Name", "Addresses"]], // ExpandAsterisk = false
+//                ["select/stages/case3_with_nested_complexType_circular_reference.json"       ,  "edm/edm2_complextype_with_circular_reference.xml"  , ["plainString", "Name", "Addresses/Street", "Addresses/ZipCode", "Addresses/BackUpAddresses/ZipCode"]], // ExpandAsterisk = false
+//                ["select/stages/case3_with_nested_complexType_circular_reference.json"       ,  "edm/edm3_complextype_with_circular_reference_collection.xml"  , ["plainString", "Name", "Addresses/Street", "Addresses/ZipCode", "Addresses/BackUpAddresses/ZipCode"]] // ExpandAsterisk = false
+        ]
+    }
+
+
+
+    // TODO ExpandAsterisk = true (all fields defined in EDM)
+}
