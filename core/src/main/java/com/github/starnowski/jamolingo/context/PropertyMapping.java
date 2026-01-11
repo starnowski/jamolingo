86e0540b52646da7c9aad64f2ba5c33451eb0b4d
starnowski <33316705+starnowski@users.noreply.github.com>
2026-01-11 15:00:05 +0100

    #1 - Added todo comment

--- core/src/main/java/com/github/starnowski/jamolingo/context/PropertyMapping.java
+++ core/src/main/java/com/github/starnowski/jamolingo/context/PropertyMapping.java
@@ -139,6 +139,10 @@ public class PropertyMapping {
     this.flattenedLevelUp = flattenedLevelUp;
   }
 
+
+  /**
+   * TODO Add Github issue relate to handling circular reference
+   */
   @JsonProperty("flattenedLevelUp")
   private Integer flattenedLevelUp;
 
