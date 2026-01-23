476cb6da903e88cf9dec30547b6d208b6d40ba30
starnowski <33316705+starnowski@users.noreply.github.com>
2026-01-23 21:24:00 +0100

    #13 - Added tests cases [skip ci]

--- perf/src/test/java/com/github/starnowski/jamolingo/perf/ExplainAnalyzeResultFactoryIndexMatchStageResolvingTest.java
+++ perf/src/test/java/com/github/starnowski/jamolingo/perf/ExplainAnalyzeResultFactoryIndexMatchStageResolvingTest.java
@@ -116,6 +116,26 @@ class ExplainAnalyzeResultFactoryIndexMatchStageResolvingTest {
         "results/or_query_result.json");
   }
 
+  @Test
+  @MongoSetup(
+      mongoDocuments = {
+        @MongoDocument(
+            database = TEST_DATABASE,
+            collection = "docs",
+            bsonFilePath = "data/doc1.json"),
+        @MongoDocument(
+            database = TEST_DATABASE,
+            collection = "docs",
+            bsonFilePath = "data/doc_collscan.json")
+      })
+  public void shouldResolveCorrectIndexValueForOrQueryWithCollScan() throws IOException {
+    shouldResolveCorrectIndexValueAndReturnCorrectData(
+        DEFAULT_INDEXES,
+        "pipelines/or_collscan_query.json",
+        "COLLSCAN",
+        "results/or_collscan_query_result.json");
+  }
+
   private void shouldResolveCorrectIndexValueAndReturnCorrectData(
       List<Document> indexes,
       String pipelineFilePath,
