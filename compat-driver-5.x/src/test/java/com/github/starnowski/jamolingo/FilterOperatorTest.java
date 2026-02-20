dcf175886b3614d89ea19f4c7693ebd00f07202f
starnowski <33316705+starnowski@users.noreply.github.com>
2026-02-20 19:52:57 +0100

    #17 - Added tests cases

--- compat-driver-5.x/src/test/java/com/github/starnowski/jamolingo/FilterOperatorTest.java
+++ compat-driver-5.x/src/test/java/com/github/starnowski/jamolingo/FilterOperatorTest.java
@@ -116,6 +116,47 @@ class FilterOperatorTest extends AbstractItTest {
         Arguments.of("plainString in ('Some text', 'no such text')", Set.of("Some text")),
         Arguments.of("startswith(plainString,'So')", Set.of("Some text")),
         Arguments.of(
-            "startswith(plainString,'So') and plainString eq 'Some text'", Set.of("Some text")));
+            "startswith(plainString,'So') and plainString eq 'Some text'", Set.of("Some text")),
+        Arguments.of(
+            "startswith(plainString,'Some t') and smallInteger eq -1188957731", Set.of("Some text")),
+        Arguments.of("startswith(plainString,'Po') or smallInteger eq -113", Set.of("Poem")),
+        Arguments.of(
+            "timestamp ge 2024-07-20T10:00:00.00Z and timestamp le 2024-07-20T20:00:00.00Z",
+            Set.of("eOMtThyhVNLWUZNRcBaQKxI")),
+        Arguments.of(
+            "plainString eq 'eOMtThyhVNLWUZNRcBaQKxI' and uuidProp eq b921f1dd-3cbc-0495-fdab-8cd14d33f0aa",
+            Set.of("eOMtThyhVNLWUZNRcBaQKxI")),
+        Arguments.of(
+            "plainString eq 'eOMtThyhVNLWUZNRcBaQKxI' and password eq 'password1'",
+            Set.of("eOMtThyhVNLWUZNRcBaQKxI")),
+        Arguments.of(
+            "plainString eq 'eOMtThyhVNLWUZNRcBaQKxI' or password eq 'password1'",
+            Set.of("eOMtThyhVNLWUZNRcBaQKxI")),
+        Arguments.of("tags/any(t:t eq 'developer') or tags/any(t:t eq 'LLM')", Set.of("Poem")),
+        Arguments.of("tags/any(t:startswith(t,'dev'))", Set.of("Poem")),
+        Arguments.of("tags/any(t:startswith(t,'dev') and length(t) eq 9)", Set.of("Poem")),
+        Arguments.of("tags/any(t:length(t) eq 13)", Set.of("eOMtThyhVNLWUZNRcBaQKxI")),
+        Arguments.of("tags/any(t:tolower(t) eq 'developer')", Set.of("Poem")),
+        Arguments.of(
+            "tags/any(t:startswith(t,'spider') and endswith(t, 'web'))", Set.of("Some text")),
+        Arguments.of(
+            "year(birthDate) eq 2024", Set.of("eOMtThyhVNLWUZNRcBaQKxI", "Some text", "Poem")),
+        Arguments.of("month(birthDate) eq 6", Set.of("eOMtThyhVNLWUZNRcBaQKxI", "Some text", "Poem")),
+        Arguments.of("day(birthDate) eq 18", Set.of("eOMtThyhVNLWUZNRcBaQKxI", "Some text", "Poem")),
+        Arguments.of(
+            "hour(timestamp) eq 10", Set.of("eOMtThyhVNLWUZNRcBaQKxI", "Some text", "Poem")),
+        Arguments.of(
+            "ceiling(floatValue) eq 1", Set.of("eOMtThyhVNLWUZNRcBaQKxI", "Some text", "Poem")),
+        Arguments.of("tags/$count ge 2", Set.of("eOMtThyhVNLWUZNRcBaQKxI", "Some text", "Poem")),
+        Arguments.of("tags/$count ge 3", Set.of("Poem")),
+        Arguments.of(
+            "trim('   Poem   ') eq 'Poem'",
+            Set.of("eOMtThyhVNLWUZNRcBaQKxI", "Some text", "Poem")),
+        Arguments.of(
+            "round(floatValue) eq 1", Set.of("eOMtThyhVNLWUZNRcBaQKxI", "Some text", "Poem")),
+        Arguments.of(
+            "minute(timestamp) eq 15", Set.of("eOMtThyhVNLWUZNRcBaQKxI", "Some text", "Poem")),
+        Arguments.of(
+            "second(timestamp) eq 26", Set.of("eOMtThyhVNLWUZNRcBaQKxI", "Some text", "Poem")));
   }
 }
