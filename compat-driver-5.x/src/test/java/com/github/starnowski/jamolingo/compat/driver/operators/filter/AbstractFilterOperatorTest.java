e94ab727e1195816cd9b77c531fa2619583be62d
starnowski <33316705+starnowski@users.noreply.github.com>
2026-03-01 15:18:37 +0100

    #17 - Tests code refactoring [skip ci]

--- compat-driver-5.x/src/test/java/com/github/starnowski/jamolingo/compat/driver/operators/filter/AbstractFilterOperatorTest.java
+++ compat-driver-5.x/src/test/java/com/github/starnowski/jamolingo/compat/driver/operators/filter/AbstractFilterOperatorTest.java
@@ -1,7 +1,5 @@
 package com.github.starnowski.jamolingo.compat.driver.operators.filter;
 
-import com.github.starnowski.jamolingo.AbstractItTest;
-import com.github.starnowski.jamolingo.EmbeddedMongoResource;
 import com.github.starnowski.jamolingo.core.operators.filter.FilterOperatorResult;
 import com.github.starnowski.jamolingo.core.operators.filter.ODataFilterToMongoMatchParser;
 import com.github.starnowski.jamolingo.perf.ExplainAnalyzeResult;
@@ -9,14 +7,7 @@ import com.github.starnowski.jamolingo.perf.ExplainAnalyzeResultFactory;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import io.quarkus.test.common.QuarkusTestResource;
-import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 import javax.xml.stream.XMLStreamException;
@@ -32,9 +23,7 @@ import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.junit.jupiter.api.Assertions;
 
-@QuarkusTest
-@QuarkusTestResource(EmbeddedMongoResource.class)
-public abstract class AbstractFilterOperatorTest extends AbstractItTest {
+public abstract class AbstractFilterOperatorTest extends AbstractBaseFilterOperatorTest {
 
   @Inject protected MongoClient mongoClient;
 
@@ -103,58 +92,5 @@ public abstract class AbstractFilterOperatorTest extends AbstractItTest {
 
     // THEN
     Assertions.assertEquals(expectedIndex, explainResult.getIndexValue().getValue());
-  }
-
-  protected void createIndexesForPropertyInCollection(
-      String database, String collectionName, Set<String> properties) {
-    properties.forEach(pro -> createIndexForPropertyInCollection(database, collectionName, pro));
-  }
-
-  protected void createIndexForPropertyInCollection(
-      String database, String collectionName, String property) {
-    MongoCollection<Document> col = mongoClient.getDatabase(database).getCollection(collectionName);
-    col.createIndex(new Document(property, 1));
-  }
-
-  protected void dropIndexesForCollections(String database, Set<String> collectionNames) {
-    collectionNames.forEach(col -> dropIndexesForCollection(database, col));
-  }
-
-  protected void dropIndexesForCollection(String database, String collectionName) {
-    try {
-
-      MongoCollection<Document> col =
-          mongoClient.getDatabase(database).getCollection(collectionName);
-      col.dropIndexes();
-    } catch (Exception exception) {
-      // Do nothing
-      exception.printStackTrace();
-    }
-  }
-
-  private void logTestsObject(String filterString, List<Bson> pipeline) {
-    String path =
-        Paths.get(new File(getClass().getClassLoader().getResource(".").getFile()).getPath())
-            .toAbsolutePath()
-            .toString();
-    System.out.println("File path is " + path);
-    try (FileOutputStream outputStream =
-        new FileOutputStream(path + File.separator + "testcases.txt", true)) {
-      outputStream.write("<test>".getBytes(StandardCharsets.UTF_8));
-      outputStream.write(
-          ("<filter>$filter=" + filterString + "</filter>").getBytes(StandardCharsets.UTF_8));
-      outputStream.write(
-          ("<pipeline>"
-                  + pipeline
-                      .get(0)
-                      .toBsonDocument(Document.class, this.mongoClient.getCodecRegistry())
-                      .toJson()
-                  + "</pipeline>")
-              .getBytes(StandardCharsets.UTF_8));
-      outputStream.write("</test>".getBytes(StandardCharsets.UTF_8));
-      outputStream.write("\n".getBytes(StandardCharsets.UTF_8));
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
   }
 }
