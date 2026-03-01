788f82634d7e4b9f20223d117423086f6b1f27c9
starnowski <33316705+starnowski@users.noreply.github.com>
2026-03-01 14:18:46 +0100

    #17 - Added tests (not yet finished!) [skip ci]

--- compat-driver-5.x/src/test/java/com/github/starnowski/jamolingo/compat/driver/operators/filter/FilterOperatorTest.java
+++ compat-driver-5.x/src/test/java/com/github/starnowski/jamolingo/compat/driver/operators/filter/FilterOperatorTest.java
@@ -69,6 +69,48 @@ public class FilterOperatorTest extends AbstractFilterOperatorTest {
     shouldReturnExpectedDocumentsBasedOnFilterOperator(filter, expectedPlainStrings);
   }
 
+  @ParameterizedTest
+  @MethodSource("provideShouldReturnExpectedProjectedDocument")
+  @MongoSetup(
+          mongoDocuments = {
+                  @MongoDocument(
+                          database = "testdb",
+                          collection = "Items",
+                          bsonFilePath = "bson/filter/example2_1.json"),
+                  @MongoDocument(
+                          database = "testdb",
+                          collection = "Items",
+                          bsonFilePath = "bson/filter/example2_2.json"),
+                  @MongoDocument(
+                          database = "testdb",
+                          collection = "Items",
+                          bsonFilePath = "bson/filter/example2_3.json"),
+                  @MongoDocument(
+                          database = "testdb",
+                          collection = "Items",
+                          bsonFilePath = "bson/filter/example2_4.json"),
+                  @MongoDocument(
+                          database = "testdb",
+                          collection = "Items",
+                          bsonFilePath = "bson/filter/example2_5.json"),
+                  @MongoDocument(
+                          database = "testdb",
+                          collection = "Items",
+                          bsonFilePath = "bson/filter/example2_6.json"),
+                  @MongoDocument(
+                          database = "testdb",
+                          collection = "Items",
+                          bsonFilePath = "bson/filter/example2_7.json")
+          })
+  public void shouldUsedExpectedIndexesBasedOnFilterOperator(String filter, Set<String> expectedPlainStrings, String expectedIndex)
+          throws UriValidationException,
+          UriParserException,
+          XMLStreamException,
+          ExpressionVisitException,
+          ODataApplicationException {
+    shouldUsedExpectedIndexesBasedOnFilterOperator(filter, expectedIndex);
+  }
+
   private static Stream<Arguments> provideShouldReturnExpectedProjectedDocument() {
     return Stream.of(
         Arguments.of(
