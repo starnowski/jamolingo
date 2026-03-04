722fa25238df9574098116fc9e9cfc737b7216f4
starnowski <33316705+starnowski@users.noreply.github.com>
2026-03-04 01:08:01 +0100

    #24 - Added tests case [skip ci]

--- core/src/test/groovy/com/github/starnowski/jamolingo/core/operators/filter/ODataFilterToMongoMatchParserWithOverrideLiteralConverterTest.groovy
+++ core/src/test/groovy/com/github/starnowski/jamolingo/core/operators/filter/ODataFilterToMongoMatchParserWithOverrideLiteralConverterTest.groovy
@@ -5,6 +5,7 @@ import com.mongodb.MongoClientSettings
 import org.apache.olingo.commons.api.edm.Edm
 import org.apache.olingo.server.api.OData
 import org.apache.olingo.server.api.uri.UriInfo
+import org.apache.olingo.server.api.uri.queryoption.expression.Literal
 import org.apache.olingo.server.core.uri.parser.Parser
 import org.bson.Document
 import org.bson.UuidRepresentation
@@ -37,9 +38,40 @@ class ODataFilterToMongoMatchParserWithOverrideLiteralConverterTest extends Abst
                             "\$filter=" +filter
                             , null, null)
             ODataFilterToMongoMatchParser tested = new ODataFilterToMongoMatchParser()
+//            ODataToBsonConverter oDataToBsonConverter = new DefaultODataToBsonConverter() {
+//                @Override
+//                Object toBsonValue(Object value, String type) {
+//                    def valToUnwrap = value
+//                    if (value instanceof org.bson.BsonString) {
+//                        valToUnwrap = value.asString().getValue()
+//                    }
+//                    if (valToUnwrap instanceof String && valToUnwrap.startsWith("custom_string(") && valToUnwrap.endsWith(")")) {
+//                        valToUnwrap = valToUnwrap.substring("custom_string(".length(), valToUnwrap.length() - 1)
+//                    }
+//                    return super.toBsonValue(valToUnwrap, type)
+//                }
+//            }
+            LiteralToBsonConverter literalToBsonConverter = new DefaultLiteralToBsonConverter() {
+                @Override
+                Bson convert(Literal literal) {
+                    Bson result = super.convert(literal)
+                    if (result instanceof Document && result.containsKey('$odata.literal')) {
+                        def value = result.get('$odata.literal')
+                        if (value instanceof String && value.startsWith("custom_string(") && value.endsWith(")")) {
+                            value = value.substring("custom_string(".length(), value.length() - 1)
+                            return new Document('$odata.literal', value)
+                        }
+                    }
+                    return result
+                }
+            }
+            MongoFilterVisitorCommonContext context = DefaultMongoFilterVisitorCommonContext.builder()
+//                    .withODataToBsonConverter(oDataToBsonConverter)
+                    .withLiteralToBsonConverter(literalToBsonConverter)
+                    .build()
 
         when:
-            def result = tested.parse(uriInfo.getFilterOption())
+            def result = tested.parse(uriInfo.getFilterOption(), context)
 
         then:
             [((Document)result.getStageObjects().get(0)).toJson(settings, codec)] == [((Document)expectedBson).toJson(settings, codec)]
