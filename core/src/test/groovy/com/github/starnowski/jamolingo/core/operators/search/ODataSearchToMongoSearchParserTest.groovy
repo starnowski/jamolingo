31130fd16895ec0bcb79775cf52fe3440c486685
starnowski <33316705+starnowski@users.noreply.github.com>
2026-03-11 22:32:54 +0100

    #25 - Added unit tests [skip ci]

--- core/src/test/groovy/com/github/starnowski/jamolingo/core/operators/search/ODataSearchToMongoSearchParserTest.groovy
+++ core/src/test/groovy/com/github/starnowski/jamolingo/core/operators/search/ODataSearchToMongoSearchParserTest.groovy
@@ -58,14 +58,25 @@ class ODataSearchToMongoSearchParserTest extends AbstractSpecification {
         [((Document)result.getStageObjects().get(0)).toJson(settings, codec)] == [expectedBson.toJson(settings, codec)]
 
         where:
-            searchValue                     ||  expectedBsonJson
-            """database AND search"""       || """{ "\$search": { "index": "default", "queryString": { "query": "database AND search", "path": ["name","description"] }}}"""
-            """database OR search"""        || """{ "\$search": { "index": "default", "queryString": { "query": "database OR search", "path": ["name","description"] }}}"""
-            """database NOT legacy"""       || """{ "\$search": { "index": "default", "queryString": { "query": "database NOT legacy", "path": ["name","description"] }}}"""
-            """\"AND\""""                   || """{ "\$search": { "index": "default", "queryString": { "query": "\\"AND\\"", "path": ["name","description"] }}}"""
-            """\"OR\""""                    || """{ "\$search": { "index": "default", "queryString": { "query": "\\"OR\\"", "path": ["name","description"] }}}"""
-            """\"NOT\""""                   || """{ "\$search": { "index": "default", "queryString": { "query": "\\"NOT\\"", "path": ["name","description"] }}}"""
-            """\"AND operator\""""          || """{ "\$search": { "index": "default", "queryString": { "query": "\\"AND operator\\"", "path": ["name","description"] }}}"""
+            searchValue                                             ||  expectedBsonJson
+            """database AND search"""                               || """{ "\$search": { "index": "default", "queryString": { "query": "database AND search", "path": ["name","description"] }}}"""
+            """database OR search"""                                || """{ "\$search": { "index": "default", "queryString": { "query": "database OR search", "path": ["name","description"] }}}"""
+            """database NOT legacy"""                               || """{ "\$search": { "index": "default", "queryString": { "query": "database NOT legacy", "path": ["name","description"] }}}"""
+            """\"AND\""""                                           || """{ "\$search": { "index": "default", "queryString": { "query": "\\"AND\\"", "path": ["name","description"] }}}"""
+            """\"OR\""""                                            || """{ "\$search": { "index": "default", "queryString": { "query": "\\"OR\\"", "path": ["name","description"] }}}"""
+            """\"NOT\""""                                           || """{ "\$search": { "index": "default", "queryString": { "query": "\\"NOT\\"", "path": ["name","description"] }}}"""
+            """\"AND operator\""""                                  || """{ "\$search": { "index": "default", "queryString": { "query": "\\"AND operator\\"", "path": ["name","description"] }}}"""
+            """\"rock AND roll\""""                                 || """{ "\$search": { "index": "default", "queryString": { "query": "\\"rock AND roll\\"", "path": ["name","description"] }}}"""
+            """"OR condition" AND database"""                       || """{ "\$search": { "index": "default", "queryString": { "query": "\\"OR condition\\" AND database", "path": ["name","description"] }}}"""
+            """"NOT operator" OR logic"""                           || """{ "\$search": { "index": "default", "queryString": { "query": "\\"NOT operator\\" OR logic", "path": ["name","description"] }}}"""
+            """(database OR search) AND index"""                    || """{ "\$search": { "index": "default", "queryString": { "query": "(database OR search) AND index", "path": ["name","description"] }}}"""
+            """("AND" OR "OR") AND logic"""                         || """{ "\$search": { "index": "default", "queryString": { "query": "(\\"AND\\" OR \\"OR\\") AND logic", "path": ["name","description"] }}}"""
+            """(database OR "AND") AND system"""                    || """{ "\$search": { "index": "default", "queryString": { "query": "(database OR \\"AND\\") AND system", "path": ["name","description"] }}}"""
+            """\"database OR search\""""                            || """{ "\$search": { "index": "default", "queryString": { "query": "\\"database OR search\\"", "path": ["name","description"] }}}"""
+            """\"AND OR NOT\""""                                    || """{ "\$search": { "index": "default", "queryString": { "query": "\\"AND OR NOT\\"", "path": ["name","description"] }}}"""
+            """database AND ("OR" OR "NOT")"""                      || """{ "\$search": { "index": "default", "queryString": { "query": "database AND (\\"OR\\" OR \\"NOT\\")", "path": ["name","description"] }}}"""
+            """\"logical AND operator\""""                          || """{ "\$search": { "index": "default", "queryString": { "query": "\\"logical AND operator\\"", "path": ["name","description"] }}}"""
+            """("database search" OR "full text") AND engine"""     || """{ "\$search": { "index": "default", "queryString": { "query": "(\\"database search\\" OR \\"full text\\") AND engine", "path": ["name","description"] }}}"""
     }
 
 
