35333b382188624d32fd661e0b1c3e2bd7d00eb5
starnowski <33316705+starnowski@users.noreply.github.com>
2026-03-11 22:58:40 +0100

    #25 - Added implementation of parsed queryString [skip ci]

--- core/src/main/java/com/github/starnowski/jamolingo/core/operators/search/SearchDocumentForQueryStringFactory.java
+++ core/src/main/java/com/github/starnowski/jamolingo/core/operators/search/SearchDocumentForQueryStringFactory.java
@@ -1,6 +1,11 @@
 package com.github.starnowski.jamolingo.core.operators.search;
 
+import org.apache.olingo.server.api.uri.queryoption.search.SearchBinary;
+import org.apache.olingo.server.api.uri.queryoption.search.SearchBinaryOperatorKind;
 import org.apache.olingo.server.api.uri.queryoption.search.SearchExpression;
+import org.apache.olingo.server.api.uri.queryoption.search.SearchTerm;
+import org.apache.olingo.server.api.uri.queryoption.search.SearchUnary;
+import org.apache.olingo.server.api.uri.queryoption.search.SearchUnaryOperatorKind;
 import org.bson.conversions.Bson;
 
 public abstract class SearchDocumentForQueryStringFactory implements SearchDocumentFactory {
@@ -11,15 +16,52 @@ public abstract class SearchDocumentForQueryStringFactory implements SearchDocum
 
   private QueryStringParsingResult pares(SearchExpression searchExpression) {
     try {
-      return new QueryStringParsingResult("", true, null);
+      return new QueryStringParsingResult(parseSearchExpressionToString(searchExpression), true, null);
     } catch (Exception ex) {
       return new QueryStringParsingResult(null, false, ex);
     }
   }
 
   protected String parseSearchExpressionToString(SearchExpression searchExpression) {
-    // TODO
+    if (searchExpression instanceof SearchTerm) {
+      return formatTerm(((SearchTerm) searchExpression).getSearchTerm());
+    } else if (searchExpression instanceof SearchBinary) {
+      SearchBinary binary = (SearchBinary) searchExpression;
+      String left = parseSearchExpressionToString(binary.getLeftOperand());
+      String right = parseSearchExpressionToString(binary.getRightOperand());
+
+      if (binary.getLeftOperand() instanceof SearchBinary) {
+        left = "(" + left + ")";
+      }
+
+      if (binary.getRightOperand() instanceof SearchBinary) {
+        right = "(" + right + ")";
+      }
+
+      if (binary.getOperator() == SearchBinaryOperatorKind.AND
+          && binary.getRightOperand() instanceof SearchUnary) {
+        if (((SearchUnary) binary.getRightOperand()).getOperator() == SearchUnaryOperatorKind.NOT) {
+          return left + " " + right;
+        }
+      }
+
+      return left + " " + binary.getOperator().toString() + " " + right;
+    } else if (searchExpression instanceof SearchUnary) {
+      SearchUnary unary = (SearchUnary) searchExpression;
+      String operand = parseSearchExpressionToString(unary.getOperand());
+      if (unary.getOperand() instanceof SearchBinary) {
+        operand = "(" + operand + ")";
+      }
+      return "NOT " + operand;
+    }
     return "";
+  }
+
+  private String formatTerm(String term) {
+    if (term.contains(" ") || term.equals("AND") || term.equals("OR") || term.equals("NOT")) {
+      return "\"" + term.replace("\\", "\\\\").replace("\"", "\\\\\"") + "\"";
+    }
+    return term;
   }
 
   public abstract Bson build(
