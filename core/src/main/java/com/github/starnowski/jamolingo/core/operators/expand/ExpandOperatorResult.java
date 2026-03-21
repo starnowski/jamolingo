6e337b1a472c0b9d7840ce258d2782d6e16ef351
starnowski <33316705+starnowski@users.noreply.github.com>
2026-03-21 15:00:38 +0100

    #35 - Added basic components [skip ci]

---
+++ core/src/main/java/com/github/starnowski/jamolingo/core/operators/expand/ExpandOperatorResult.java
@@ -0,0 +1,6 @@
+package com.github.starnowski.jamolingo.core.operators.expand;
+
+import com.github.starnowski.jamolingo.core.operators.OlingoOperatorResult;
+
+/** Represents the result of processing an OData $expand system query option. */
+public interface ExpandOperatorResult extends OlingoOperatorResult {}
---
+++ core/src/main/java/com/github/starnowski/jamolingo/core/operators/expand/ODataExpandToMongoAggregationPipelineParser.java
@@ -0,0 +1,11 @@
+package com.github.starnowski.jamolingo.core.operators.expand;
+
+import org.apache.olingo.server.api.uri.queryoption.ExpandOption;
+
+public class ODataExpandToMongoAggregationPipelineParser {
+
+  ExpandOperatorResult parse(ExpandOption expandOption, ExpandParserContext expandParserContext) {
+    // TODO
+    return null;
+  }
+}
