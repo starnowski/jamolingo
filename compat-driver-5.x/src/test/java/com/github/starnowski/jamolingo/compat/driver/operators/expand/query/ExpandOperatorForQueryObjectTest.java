abcb5bfac82dd1647dc833cd3405375195fb6182
starnowski <33316705+starnowski@users.noreply.github.com>
2026-03-24 23:38:27 +0100

    #35 - Tests code refactoring

--- compat-driver-5.x/src/test/java/com/github/starnowski/jamolingo/compat/driver/operators/expand/query/ExpandOperatorForQueryObjectTest.java
+++ compat-driver-5.x/src/test/java/com/github/starnowski/jamolingo/compat/driver/operators/expand/query/ExpandOperatorForQueryObjectTest.java
@@ -1,5 +1,7 @@
 package com.github.starnowski.jamolingo.compat.driver.operators.expand.query;
 
+import static com.github.starnowski.jamolingo.compat.driver.operators.filter.FilterTestsCasesAggregator.provideShouldReturnExpectedProjectedDocumentForComplexListForAllLambda;
+
 import com.github.starnowski.jamolingo.common.beans.KeyValue;
 import com.github.starnowski.jamolingo.compat.driver.operators.filter.FilterTestsCasesAggregator;
 import com.github.starnowski.jamolingo.junit5.MongoDocument;
@@ -14,6 +16,7 @@ import org.apache.olingo.server.api.ODataApplicationException;
 import org.apache.olingo.server.api.uri.queryoption.expression.ExpressionVisitException;
 import org.apache.olingo.server.core.uri.parser.UriParserException;
 import org.apache.olingo.server.core.uri.validator.UriValidationException;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -30,6 +33,14 @@ public class ExpandOperatorForQueryObjectTest extends AbstractExpandOperatorForQ
           "Oleksa",
           "example1",
           "example2");
+  public static final Map<@NotNull KeyValue<String, String>, @NotNull KeyValue<String, String>>
+      EDM_TABLES_TO_MONGO_DB_COLLECTIONS =
+          Map.of(
+              new KeyValue<>("MyService", "Category"),
+              new KeyValue<>("testdb", "categories"),
+              new KeyValue<>("MyService", "Example2"),
+              new KeyValue<>("testdb", "examples"));
+  public static final int ROOT_DOCUMENT_ID = 100;
 
   private static Stream<Arguments> provideShouldReturnExpectedProjectedDocument() {
     return FilterTestsCasesAggregator
@@ -38,6 +49,10 @@ public class ExpandOperatorForQueryObjectTest extends AbstractExpandOperatorForQ
 
   public static Stream<Arguments> provideShouldReturnExpectedProjectedDocumentForAllLambda() {
     return FilterTestsCasesAggregator.provideShouldReturnExpectedProjectedDocumentForAllLambda();
+  }
+
+  private static Stream<Arguments> provideShouldReturnExpectedProjectedDocumentForComplexList() {
+    return provideShouldReturnExpectedProjectedDocumentForComplexListForAllLambda();
   }
 
   @ParameterizedTest
@@ -89,14 +104,7 @@ public class ExpandOperatorForQueryObjectTest extends AbstractExpandOperatorForQ
           ExpressionVisitException,
           ODataApplicationException {
     shouldReturnExpectedDocumentsBasedOnQueryObjectForFilterOperator(
-        filter,
-        expectedPlainStrings,
-        Map.of(
-            new KeyValue<>("MyService", "Category"),
-            new KeyValue<>("testdb", "categories"),
-            new KeyValue<>("MyService", "Example2"),
-            new KeyValue<>("testdb", "examples")),
-        100);
+        filter, expectedPlainStrings, EDM_TABLES_TO_MONGO_DB_COLLECTIONS, ROOT_DOCUMENT_ID);
   }
 
   @ParameterizedTest
@@ -150,13 +158,56 @@ public class ExpandOperatorForQueryObjectTest extends AbstractExpandOperatorForQ
     String filterString =
         filter instanceof String ? (String) filter : String.join(" and ", (List<String>) filter);
     shouldReturnExpectedDocumentsBasedOnQueryObjectForFilterOperator(
-        filterString,
-        expectedPlainStrings,
-        Map.of(
-            new KeyValue<>("MyService", "Category"),
-            new KeyValue<>("testdb", "categories"),
-            new KeyValue<>("MyService", "Example2"),
-            new KeyValue<>("testdb", "examples")),
-        100);
+        filterString, expectedPlainStrings, EDM_TABLES_TO_MONGO_DB_COLLECTIONS, ROOT_DOCUMENT_ID);
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideShouldReturnExpectedProjectedDocumentForComplexList")
+  @MongoSetup(
+      mongoDocuments = {
+        @MongoDocument(
+            database = "testdb",
+            collection = "examples",
+            bsonFilePath = "bson/expand/query/example2_complex_1.json"),
+        @MongoDocument(
+            database = "testdb",
+            collection = "examples",
+            bsonFilePath = "bson/expand/query/example2_complex_2.json"),
+        @MongoDocument(
+            database = "testdb",
+            collection = "examples",
+            bsonFilePath = "bson/expand/query/example2_complex_3.json"),
+        @MongoDocument(
+            database = "testdb",
+            collection = "examples",
+            bsonFilePath = "bson/expand/query/example2_complex_4.json"),
+        @MongoDocument(
+            database = "testdb",
+            collection = "examples",
+            bsonFilePath = "bson/expand/query/example2_complex_5.json"),
+        @MongoDocument(
+            database = "testdb",
+            collection = "examples",
+            bsonFilePath = "bson/expand/query/example2_complex_6.json"),
+        @MongoDocument(
+            database = "testdb",
+            collection = "examples",
+            bsonFilePath = "bson/expand/query/example2_only_id.json"),
+        @MongoDocument(
+            database = "testdb",
+            collection = "examples",
+            bsonFilePath = "bson/expand/query/example2_root.json")
+      })
+  public void shouldReturnExpectedDocumentsForComplexList(
+      Object filter, Set<String> expectedPlainStrings, String expectedIndex)
+      throws UriValidationException,
+          UriParserException,
+          XMLStreamException,
+          ExpressionVisitException,
+          ODataApplicationException {
+    String filterString =
+        filter instanceof String ? (String) filter : String.join(" and ", (List<String>) filter);
+    shouldReturnExpectedDocumentsBasedOnQueryObjectForFilterOperator(
+        filterString, expectedPlainStrings, EDM_TABLES_TO_MONGO_DB_COLLECTIONS, ROOT_DOCUMENT_ID);
   }
 }
