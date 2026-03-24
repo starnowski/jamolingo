94ee76693773f537667ed333363a1dff3fcc10fb
starnowski <33316705+starnowski@users.noreply.github.com>
2026-03-24 20:29:32 +0100

    #35 - Tests code refactoring [skip ci]

--- compat-driver-5.x/src/test/java/com/github/starnowski/jamolingo/compat/driver/operators/filter/FilterTestsCasesAggregator.java
+++ compat-driver-5.x/src/test/java/com/github/starnowski/jamolingo/compat/driver/operators/filter/FilterTestsCasesAggregator.java
@@ -1,5 +1,7 @@
 package com.github.starnowski.jamolingo.compat.driver.operators.filter;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
 import org.junit.jupiter.params.provider.Arguments;
@@ -149,5 +151,226 @@ public class FilterTestsCasesAggregator {
             "tags/all(t:contains(t,'starlord') or contains(t,'trek') or contains(t,'wars')) and tags/any()",
             Set.of("Mario", "Oleksa"),
             "COLLSCAN"));
+  }
+
+  ///  ANY LAMBDA FILTERS
+  public static Stream<Arguments> provideShouldReturnExpectedProjectedDocumentForAnyLambda() {
+    return Stream.of(
+        Arguments.of(
+            "tags/any(t:t eq 'word wide web')",
+            Set.of("eOMtThyhVNLWUZNRcBaQKxI"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "tags/any(t:startswith(t,'star'))", Set.of("Mario", "Oleksa"), "FETCH + IXSCAN"),
+        Arguments.of(
+            "tags/any(t:contains(t,'spider'))",
+            Set.of("eOMtThyhVNLWUZNRcBaQKxI", "Some text"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "numericArray/any(n:n gt 25)",
+            Set.of("eOMtThyhVNLWUZNRcBaQKxI", "Mario", "Oleksa"),
+            "FETCH + IXSCAN"),
+        Arguments.of("numericArray/any(n:n lt 10)", Set.of("Some text", "Poem"), "FETCH + IXSCAN"),
+        Arguments.of(
+            "tags/any(t:contains(tolower(t),'star'))", Set.of("Mario", "Oleksa"), "COLLSCAN"),
+        Arguments.of(
+            "tags/any(t:endswith(toupper(t),'TRAP'))",
+            Set.of("eOMtThyhVNLWUZNRcBaQKxI"),
+            "COLLSCAN"),
+        Arguments.of(
+            "tags/any(t:length(t) eq 8)", Set.of("Some text", "Poem", "Oleksa"), "COLLSCAN"),
+        Arguments.of("numericArray/any(n:n add 2 gt 100)", Set.of("Mario"), "COLLSCAN"),
+        Arguments.of(
+            List.of("tags/any(t:t ne 'no such text' and t ne 'no such word')"),
+            Set.of("eOMtThyhVNLWUZNRcBaQKxI", "Some text", "Poem", "Mario", "Oleksa"),
+            "COLLSCAN"),
+        Arguments.of(
+            List.of("tags/any(t:startswith(t,'star') and t ne 'starlord')"),
+            Set.of("Mario", "Oleksa"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            List.of("tags/any(t:startswith(t,'star') or t ne 'starlord')"),
+            Set.of("eOMtThyhVNLWUZNRcBaQKxI", "Some text", "Poem", "Mario", "Oleksa"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            List.of("tags/any(t:startswith(t,'star ') or t eq 'starlord')"),
+            Set.of("Mario", "Oleksa"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            List.of("tags/any(t:startswith(t,'starlord') or t in ('star trek', 'star wars'))"),
+            Set.of("Mario", "Oleksa"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            List.of(
+                "tags/any(t:contains(t,'starlord') or contains(t,'trek') or contains(t,'wars'))"),
+            Set.of("Mario", "Oleksa"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            List.of("tags/any(t:contains(t,'starlord'))"), Set.of("Oleksa"), "FETCH + IXSCAN"),
+        Arguments.of(
+            List.of("tags/any(t:endswith(t,'web') or endswith(t,'trap'))"),
+            Set.of("eOMtThyhVNLWUZNRcBaQKxI", "Some text"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            List.of("tags/any(t:length(t) eq 9)"),
+            Set.of("Some text", "Poem", "Mario", "Oleksa"),
+            "COLLSCAN"),
+        Arguments.of(
+            List.of("numericArray/any(n:n gt 5)"),
+            Set.of("eOMtThyhVNLWUZNRcBaQKxI", "Some text", "Mario", "Oleksa"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            List.of("numericArray/any(n:n gt floor(5.05))"),
+            Set.of("eOMtThyhVNLWUZNRcBaQKxI", "Some text", "Mario", "Oleksa"),
+            "COLLSCAN"),
+        Arguments.of(
+            List.of("numericArray/any(n:n add 2 gt round(n))"),
+            Set.of("eOMtThyhVNLWUZNRcBaQKxI", "Some text", "Poem", "Mario", "Oleksa"),
+            "COLLSCAN"),
+        Arguments.of(
+            List.of("numericArray/any(n:n eq 10 or n eq 20 or n eq 30)"),
+            Set.of("eOMtThyhVNLWUZNRcBaQKxI"),
+            "FETCH + IXSCAN"),
+        // Additional tests from user list
+        Arguments.of(
+            "nestedObject/tokens/any(t:t eq 'first example') and nestedObject/numbers/any(t:t gt 5 and t lt 27)",
+            Set.of("example1"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "nestedObject/tokens/any(t:t ne 'no such text')",
+            Set.of("example1", "example2"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "tags/any(t:startswith(t,'spider') and t eq 'spiderweb')",
+            Set.of("Some text"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "tags/any(t:startswith(t,'spider') and t ne 'spiderweb' or endswith(t,'web') and t ne 'spiderweb' or contains(t,'wide') and t ne 'word wide')",
+            Set.of("eOMtThyhVNLWUZNRcBaQKxI"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "tags/any(t:startswith(t,'spider') and t ne 'spiderweb' or endswith(t,'web') and t ne 'spiderweb')",
+            Set.of("eOMtThyhVNLWUZNRcBaQKxI"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "tags/any(t:startswith(t,'spider') and t ne 'spiderweb' or endswith(t,'web') and t ne 'spiderwebgg' or contains(t,'wide') and t ne 'word wide')",
+            Set.of("eOMtThyhVNLWUZNRcBaQKxI", "Some text"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "tags/any(t:startswith(t,'spider') and t ne 'spiderweb' or startswith(t,'spider') and t ne 'spider' or contains(t,'wide') and t ne 'word wide')",
+            Set.of("eOMtThyhVNLWUZNRcBaQKxI", "Some text"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "tags/any(t:startswith(t,'spider') and t ne 'spiderweb')",
+            Set.of("eOMtThyhVNLWUZNRcBaQKxI"),
+            "FETCH + IXSCAN"));
+  }
+
+  public static Stream<Arguments>
+      provideShouldReturnExpectedProjectedDocumentForComplexListForAnyLambda() {
+    return Stream.of(
+        Arguments.of(
+            "complexList/any(c:c/someString eq 'Apple')", Set.of("Doc1", "Doc4"), "FETCH + IXSCAN"),
+        Arguments.of(
+            "complexList/any(c:c/someNumber gt 35)",
+            Set.of("Doc2", "Doc3", "Doc4", "Doc6"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "complexList/any(c:c/someString eq 'Banana' or c/someString eq 'Cherry')",
+            Set.of("Doc2", "Doc3"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "complexList/any(c:c/nestedComplexArray/any(n:n/stringVal eq 'val1'))",
+            Set.of("Doc1", "Doc2", "Doc4"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "complexList/any(c:startswith(c/someString,'Ap'))",
+            Set.of("Doc1", "Doc4", "Doc5"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "complexList/any(c:contains(c/someString,'ana'))", Set.of("Doc2"), "FETCH + IXSCAN"),
+        Arguments.of(
+            "complexList/any(c:endswith(c/someString,'erry'))",
+            Set.of("Doc3", "Doc4"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "complexList/any(c:contains(c/someString,'e'))",
+            Set.of("Doc1", "Doc3", "Doc4", "Doc6"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "complexList/any(c:c/someString eq 'Application')",
+            Set.of("Doc1", "Doc5"),
+            "FETCH + IXSCAN"),
+        // Missing complex numeric tests
+        Arguments.of(
+            "complexList/any(c:c/someNumber gt 5)",
+            Set.of("Doc1", "Doc2", "Doc3", "Doc4", "Doc5", "Doc6"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "complexList/any(c:c/someNumber gt 25)",
+            Set.of("Doc2", "Doc3", "Doc4", "Doc6"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "complexList/any(c:c/someNumber lt 25)",
+            Set.of("Doc1", "Doc4", "Doc5"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "complexList/any(c:c/someNumber eq 10 or c/someNumber eq 20)",
+            Set.of("Doc1", "Doc4", "Doc5"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "complexList/any(c:c/someNumber add 5 gt 20)",
+            Set.of("Doc1", "Doc2", "Doc3", "Doc4", "Doc5", "Doc6"),
+            "COLLSCAN"),
+        Arguments.of(
+            "complexList/any(c:c/someNumber gt floor(5.05))",
+            Set.of("Doc1", "Doc2", "Doc3", "Doc4", "Doc5", "Doc6"),
+            "COLLSCAN"),
+        Arguments.of(
+            "complexList/any(c:c/someNumber add 2 gt round(c/someNumber))",
+            Set.of("Doc1", "Doc2", "Doc3", "Doc4", "Doc5", "Doc6"),
+            "COLLSCAN"),
+        Arguments.of(
+            "complexList/any(c:c/someNumber eq 20)", Set.of("Doc1", "Doc5"), "FETCH + IXSCAN"),
+        // Missing nested complex tests
+        Arguments.of(
+            "complexList/any(c:c/nestedComplexArray/any(n:startswith(n/stringVal,'val')))",
+            Set.of("Doc1", "Doc2", "Doc4"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "complexList/any(c:c/nestedComplexArray/any(n:contains(n/stringVal,'match')))",
+            Set.of("Doc5", "Doc6"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "complexList/any(c:c/nestedComplexArray/any(n:n/stringVal eq 'val1' or n/stringVal eq 'test1'))",
+            Set.of("Doc1", "Doc2", "Doc3", "Doc4"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "complexList/any(c:c/nestedComplexArray/any(n:n/stringVal eq 'val1' or n/stringVal eq 'test1') and c/someNumber ge 20)",
+            Set.of("Doc2", "Doc3", "Doc4"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "complexList/any(c:c/nestedComplexArray/any(n:n/numberVal gt 70))",
+            Set.of("Doc6"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "complexList/any(c:c/nestedComplexArray/any(n:n/numberVal eq 71) and c/nestedComplexArray/any(n:n/numberVal eq 72))",
+            Set.of("Doc6"),
+            "COLLSCAN"),
+        Arguments.of(
+            "complexList/any(c:c/nestedComplexArray/$count ge 2)", Set.of("Doc6"), "COLLSCAN"),
+        Arguments.of(
+            "complexList/any(c:c/primitiveStringList/any(n:startswith(n,'item11')))",
+            Set.of("Doc6"),
+            "FETCH + IXSCAN"),
+        Arguments.of(
+            "complexList/any(c:c/primitiveNumberList/any(n:n gt 10))",
+            Set.of("Doc6"),
+            "FETCH + IXSCAN"),
+        // Concat test
+        Arguments.of(
+            Arrays.asList("complexList/any(c:c/someNumber gt 5)", "plainString eq 'Doc1'"),
+            Set.of("Doc1"),
+            "FETCH + IXSCAN"));
   }
 }
