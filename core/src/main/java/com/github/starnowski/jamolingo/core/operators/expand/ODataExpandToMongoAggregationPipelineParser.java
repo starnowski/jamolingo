cd48be16719ddaa736dcc4d0ed2c10d5482d6eb4
starnowski <33316705+starnowski@users.noreply.github.com>
2026-03-30 22:14:41 +0200

    #35 - Setting correct max depth

--- core/src/main/java/com/github/starnowski/jamolingo/core/operators/expand/ODataExpandToMongoAggregationPipelineParser.java
+++ core/src/main/java/com/github/starnowski/jamolingo/core/operators/expand/ODataExpandToMongoAggregationPipelineParser.java
@@ -103,6 +103,7 @@ public class ODataExpandToMongoAggregationPipelineParser {
                 .append("startWith", "$" + mongoStartWith)
                 .append("connectFromField", mongoConnectFrom)
                 .append("connectToField", mongoConnectTo)
+                .append("maxDepth", eOption.getLevelsOption().getValue())
                 .append("as", navProp.getName());
         String depthVariable = navProp.getName() + ODATA_GRAPHLOOKUP_STAGE_DEPTH_VARIABLE_SUFFIX;
         if (eOption.getFilterOption() != null) {
