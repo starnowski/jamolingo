e5a5a464738ba5854c6ab6d9364f399824255bce
starnowski <33316705+starnowski@users.noreply.github.com>
2026-07-18 11:59:43 +0200

    #35 - Fixed tests case [skip ci]

--- compat-driver-5.x/src/test/java/com/github/starnowski/jamolingo/compat/driver/operators/expand/nestedtree/ExpandOperatorWithHandlingTreeRelationsWithLookupSupportForMultiLevelTest.java
+++ compat-driver-5.x/src/test/java/com/github/starnowski/jamolingo/compat/driver/operators/expand/nestedtree/ExpandOperatorWithHandlingTreeRelationsWithLookupSupportForMultiLevelTest.java
@@ -615,16 +615,61 @@ public class ExpandOperatorWithHandlingTreeRelationsWithLookupSupportForMultiLev
             Set.of(1),
             "$expand=children($levels=max;$orderby=index desc)",
             """
-                                                            [{ "_id": 1, "index": 1, "parentId": null, "categoryId": 1,
-                                                             "children": [
-                                                             { "_id": 7, "index": 7, "parentId": 6, "categoryId": 2 },
-                                                             { "_id": 6, "index": 6, "parentId": 4, "categoryId": 2 },
-                                                             { "_id": 5, "index": 5, "parentId": 1, "categoryId": 1 },
-                                                             { "_id": 4, "index": 4, "parentId": 3, "categoryId": 2 },
-                                                             { "_id": 3, "index": 3, "parentId": 2, "categoryId": 2 },
-                                                             { "_id": 2, "index": 2, "parentId": 1, "categoryId": 1 }
-                                                             ]
-                                                            }]
+                                                            [
+                                                                {
+                                                                    "_id": 1,
+                                                                    "index": 1,
+                                                                    "parentId": null,
+                                                                    "categoryId": 1,
+                                                                    "children": [
+                                                                        {
+                                                                            "_id": 5,
+                                                                            "index": 5,
+                                                                            "parentId": 1,
+                                                                            "categoryId": 1,
+                                                                            "children": []
+                                                                        },
+                                                                        {
+                                                                            "_id": 2,
+                                                                            "index": 2,
+                                                                            "parentId": 1,
+                                                                            "categoryId": 1,
+                                                                            "children": [
+                                                                                {
+                                                                                    "_id": 3,
+                                                                                    "index": 3,
+                                                                                    "parentId": 2,
+                                                                                    "categoryId": 2,
+                                                                                    "children": [
+                                                                                        {
+                                                                                            "_id": 4,
+                                                                                            "index": 4,
+                                                                                            "parentId": 3,
+                                                                                            "categoryId": 2,
+                                                                                            "children": [
+                                                                                                {
+                                                                                                    "_id": 6,
+                                                                                                    "index": 6,
+                                                                                                    "parentId": 4,
+                                                                                                    "categoryId": 2,
+                                                                                                    "children": [
+                                                                                                        {
+                                                                                                            "_id": 7,
+                                                                                                            "index": 7,
+                                                                                                            "parentId": 6,
+                                                                                                            "categoryId": 2
+                                                                                                        }
+                                                                                                    ]
+                                                                                                }
+                                                                                            ]
+                                                                                        }
+                                                                                    ]
+                                                                                }
+                                                                            ]
+                                                                        }
+                                                                    ]
+                                                                }
+                                                            ]
                                                             """,
             JSONCompareMode.STRICT_ORDER),
         Arguments.of(
