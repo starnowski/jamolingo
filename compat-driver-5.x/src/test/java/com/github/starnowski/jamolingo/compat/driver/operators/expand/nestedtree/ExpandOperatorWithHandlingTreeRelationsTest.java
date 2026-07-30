5b7b89619766c171882ece94fcdb77aca045b8fb
starnowski <33316705+starnowski@users.noreply.github.com>
2026-07-30 08:55:59 +0200

    #35 - Fixed tests expected data[skip ci]

--- compat-driver-5.x/src/test/java/com/github/starnowski/jamolingo/compat/driver/operators/expand/nestedtree/ExpandOperatorWithHandlingTreeRelationsTest.java
+++ compat-driver-5.x/src/test/java/com/github/starnowski/jamolingo/compat/driver/operators/expand/nestedtree/ExpandOperatorWithHandlingTreeRelationsTest.java
@@ -389,16 +389,52 @@ public class ExpandOperatorWithHandlingTreeRelationsTest extends AbstractItTest 
             Set.of(1),
             "$expand=children($levels=max;$orderby=index asc)",
             """
-                                                            [{ "_id": 1, "index": 1, "parentId": null, "categoryId": 1,
-                                                             "children": [
-                                                             { "_id": 2, "index": 2, "parentId": 1, "categoryId": 1 },
-                                                             { "_id": 3, "index": 3, "parentId": 2, "categoryId": 2 },
-                                                             { "_id": 4, "index": 4, "parentId": 3, "categoryId": 2 },
-                                                             { "_id": 5, "index": 5, "parentId": 1, "categoryId": 1 },
-                                                             { "_id": 6, "index": 6, "parentId": 4, "categoryId": 2 },
-                                                             { "_id": 7, "index": 7, "parentId": 6, "categoryId": 2 }
-                                                             ]
-                                                            }]
+                                                            [
+                                                                {
+                                                                    "_id": 1,
+                                                                    "index": 1,
+                                                                    "parentId": null,
+                                                                    "categoryId": 1,
+                                                                    "children": [
+                                                                        {
+                                                                            "_id": 2,
+                                                                            "index": 2,
+                                                                            "parentId": 1,
+                                                                            "categoryId": 1
+                                                                        },
+                                                                        {
+                                                                            "_id": 5,
+                                                                            "index": 5,
+                                                                            "parentId": 1,
+                                                                            "categoryId": 1
+                                                                        },
+                                                                        {
+                                                                            "_id": 3,
+                                                                            "index": 3,
+                                                                            "parentId": 2,
+                                                                            "categoryId": 2
+                                                                        },
+                                                                        {
+                                                                            "_id": 4,
+                                                                            "index": 4,
+                                                                            "parentId": 3,
+                                                                            "categoryId": 2
+                                                                        },
+                                                                        {
+                                                                            "_id": 6,
+                                                                            "index": 6,
+                                                                            "parentId": 4,
+                                                                            "categoryId": 2
+                                                                        },
+                                                                        {
+                                                                            "_id": 7,
+                                                                            "index": 7,
+                                                                            "parentId": 6,
+                                                                            "categoryId": 2
+                                                                        }
+                                                                    ]
+                                                                }
+                                                            ]
                                                             """,
             JSONCompareMode.STRICT_ORDER),
         // Level with max=5 with asc ordering by index
@@ -407,17 +443,52 @@ public class ExpandOperatorWithHandlingTreeRelationsTest extends AbstractItTest 
             Set.of(1),
             "$expand=children($levels=max;$orderby=index desc)",
             """
-                                                            [{ "_id": 1, "index": 1, "parentId": null, "categoryId": 1,
-                                                             "children": [
-                                                             { "_id": 7, "index": 7, "parentId": 6, "categoryId": 2 },
-                                                             { "_id": 6, "index": 6, "parentId": 4, "categoryId": 2 },
-                                                             { "_id": 5, "index": 5, "parentId": 1, "categoryId": 1 },
-                                                             { "_id": 4, "index": 4, "parentId": 3, "categoryId": 2 },
-                                                             { "_id": 3, "index": 3, "parentId": 2, "categoryId": 2 },
-                                                             { "_id": 2, "index": 2, "parentId": 1, "categoryId": 1 }
-                                                             ]
-                                                            }]
-                                                            """,
+                    [
+                    	{
+                    		"_id": 1,
+                    		"index": 1,
+                    		"parentId": null,
+                    		"categoryId": 1,
+                    		"children": [
+                    			{
+                    				"_id": 5,
+                    				"index": 5,
+                    				"parentId": 1,
+                    				"categoryId": 1
+                    			},
+                    			{
+                    				"_id": 2,
+                    				"index": 2,
+                    				"parentId": 1,
+                    				"categoryId": 1
+                    			},
+                    			{
+                    				"_id": 3,
+                    				"index": 3,
+                    				"parentId": 2,
+                    				"categoryId": 2
+                    			},
+                    			{
+                    				"_id": 4,
+                    				"index": 4,
+                    				"parentId": 3,
+                    				"categoryId": 2
+                    			},
+                    			{
+                    				"_id": 6,
+                    				"index": 6,
+                    				"parentId": 4,
+                    				"categoryId": 2
+                    			},
+                    			{
+                    				"_id": 7,
+                    				"index": 7,
+                    				"parentId": 6,
+                    				"categoryId": 2
+                    			}
+                    		]
+                    	}
+                    ]""",
             JSONCompareMode.STRICT_ORDER),
         // Expand level handle by $lookup stage and order by index with asc order
         Arguments.of(
