637fab292d626f5cdbc9a9474dff99d3d8b97786
starnowski <33316705+starnowski@users.noreply.github.com>
2026-08-03 21:52:17 +0200

    #35 - Added tests cases [skip ci]

--- core/src/test/groovy/com/github/starnowski/jamolingo/core/operators/expand/ODataExpandNestedLimitTest.groovy
+++ core/src/test/groovy/com/github/starnowski/jamolingo/core/operators/expand/ODataExpandNestedLimitTest.groovy
@@ -6,6 +6,7 @@ import org.apache.olingo.commons.api.edm.Edm
 import org.apache.olingo.server.api.OData
 import org.apache.olingo.server.api.uri.UriInfo
 import org.apache.olingo.server.core.uri.parser.Parser
+import spock.lang.Unroll
 
 class ODataExpandNestedLimitTest extends AbstractSpecification {
 
@@ -49,14 +50,15 @@ class ODataExpandNestedLimitTest extends AbstractSpecification {
         result.getStageObjects().size() > 0
     }
 
-    def "should throw NestedExpandLevelExceededException when maxAllowedNestedExpandLevel is 2 and nested expand level is 3"() {
+    @Unroll
+    def "should throw NestedExpandLevelExceededException when maxAllowedNestedExpandLevel is #maxAllowedNestedExpandLevel and nested expand level is #requestedLevel"() {
         given:
         Edm edm = loadEmdProvider("edm/edm_expand.xml")
         UriInfo uriInfo = new Parser(edm, OData.newInstance())
-                .parseUri("examples2", "\$expand=children(\$expand=parent(\$expand=children))", null, null)
+                .parseUri("examples2", expandQuery, null, null)
 
         def context = DefaultExpandParserContext.builder()
-                .withMaxAllowedNestedExpandLevel(2)
+                .withMaxAllowedNestedExpandLevel(maxAllowedNestedExpandLevel)
                 .build()
 
         def parser = new ODataExpandToMongoAggregationPipelineParser()
@@ -66,9 +68,17 @@ class ODataExpandNestedLimitTest extends AbstractSpecification {
 
         then:
         def e = thrown(NestedExpandLevelExceededException)
-        e.getEdmPath() == "children.parent.children"
-        e.getRequestedLevel() == 3
-        e.getMaxLevel() == 2
-        e.getMessage() == "The requested nested expand level 3 for path 'children.parent.children' exceeds the maximum allowed level 2."
+        e.getEdmPath() == expectedEdmPath
+        e.getRequestedLevel() == requestedLevel
+        e.getMaxLevel() == maxAllowedNestedExpandLevel
+        e.getMessage() == "The requested nested expand level $requestedLevel for path '$expectedEdmPath' exceeds the maximum allowed level $maxAllowedNestedExpandLevel."
+
+        where:
+        maxAllowedNestedExpandLevel | requestedLevel | expectedEdmPath                     | expandQuery
+        2                           | 3              | "children.parent.children"          | "\$expand=children(\$expand=parent(\$expand=children))"
+        1                           | 2              | "children.parent"                   | "\$expand=children(\$expand=parent)"
+        3                           | 4              | "children.parent.children.parent"   | "\$expand=children(\$expand=parent(\$expand=children(\$expand=parent)))"
+        1                           | 2              | "parent.children"                   | "\$expand=parent(\$expand=children)"
+        2                           | 3              | "parent.children.parent"            | "\$expand=children(\$expand=parent),parent(\$expand=children(\$expand=parent))"
     }
 }
