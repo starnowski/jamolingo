17558ea385e54e76a26226e50983b7097c78496b
starnowski <33316705+starnowski@users.noreply.github.com>
2026-08-06 20:58:39 +0200

    #51 - Added tests data (not yet finished!)[skip ci]

--- compat-driver-5.x/src/test/java/com/github/starnowski/jamolingo/compat/driver/operators/expand/nestedtree/ExpandOperatorWithHandlingTreeRelationsTestProperties.java
+++ compat-driver-5.x/src/test/java/com/github/starnowski/jamolingo/compat/driver/operators/expand/nestedtree/ExpandOperatorWithHandlingTreeRelationsTestProperties.java
@@ -3,13 +3,13 @@ package com.github.starnowski.jamolingo.compat.driver.operators.expand.nestedtre
 import com.github.starnowski.jamolingo.common.beans.KeyValue;
 
 public class ExpandOperatorWithHandlingTreeRelationsTestProperties {
-  protected static final String CATEGORY_COLLECTION = "MyService.Category";
-  protected static final String TREETYPE1_COLLECTION = "MyService.TreeType1";
-  protected static final String TREETYPE2_COLLECTION = "MyService.TreeType2";
-  protected static final String TREETYPE3_COLLECTION = "MyService.TreeType3";
-  protected static final String TREETYPE4_COLLECTION = "MyService.TreeType4";
-  protected static final KeyValue<String, String> TREETYPE1_MONGO_COLLECTION_USAGE_INFO =
+  public static final String CATEGORY_COLLECTION = "MyService.Category";
+  public static final String TREETYPE1_COLLECTION = "MyService.TreeType1";
+  public static final String TREETYPE2_COLLECTION = "MyService.TreeType2";
+  public static final String TREETYPE3_COLLECTION = "MyService.TreeType3";
+  public static final String TREETYPE4_COLLECTION = "MyService.TreeType4";
+  public static final KeyValue<String, String> TREETYPE1_MONGO_COLLECTION_USAGE_INFO =
       new KeyValue<>(TREETYPE1_COLLECTION, "treeType1s");
-  protected static final KeyValue<String, String> TREETYPE2_MONGO_COLLECTION_USAGE_INFO =
+  public static final KeyValue<String, String> TREETYPE2_MONGO_COLLECTION_USAGE_INFO =
       new KeyValue<>(TREETYPE2_COLLECTION, "treeType2s");
 }
