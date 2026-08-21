8fd58f8be0f923912b5db400c56552ad43354fbe
starnowski <33316705+starnowski@users.noreply.github.com>
2026-08-21 23:03:46 +0200

    #42 - Removed redundant class

--- core/src/test/java/OlingoKinds.java
+++
@@ -1,8 +0,0 @@
-public class OlingoKinds {
-  public static void main(String[] args) {
-    for (org.apache.olingo.server.api.uri.queryoption.ApplyItem.Kind k :
-        org.apache.olingo.server.api.uri.queryoption.ApplyItem.Kind.values()) {
-      System.out.println(k.name());
-    }
-  }
-}
