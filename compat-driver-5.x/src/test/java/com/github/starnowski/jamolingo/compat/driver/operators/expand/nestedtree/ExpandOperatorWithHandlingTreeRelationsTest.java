76799be974c30d6ef1dcd1d548f43da9727b6ada
starnowski <33316705+starnowski@users.noreply.github.com>
2026-09-09 12:11:01 +0200

    #56 - Added tests case [skip ci]

--- compat-driver-5.x/src/test/java/com/github/starnowski/jamolingo/compat/driver/operators/expand/nestedtree/ExpandOperatorWithHandlingTreeRelationsTest.java
+++ compat-driver-5.x/src/test/java/com/github/starnowski/jamolingo/compat/driver/operators/expand/nestedtree/ExpandOperatorWithHandlingTreeRelationsTest.java
@@ -567,6 +567,22 @@ public class ExpandOperatorWithHandlingTreeRelationsTest extends AbstractItTest 
                                     }]
                  """,
             JSONCompareMode.NON_EXTENSIBLE),
+        //TODO
+            // Expand level handle by $lookup stage and use the $apply with order so that the
+            // NON_EXTENSIBLE mode could be used for comparison
+            // Filtering also the dynamic field like totalCount
+            Arguments.of(
+                    TREETYPE1_MONGO_COLLECTION_USAGE_INFO,
+                    Set.of(1),
+                    "$expand=treeType2s($apply=concat(filter(categoryId eq 2),groupby((categoryId),aggregate($count as totalCount)));$orderby=categoryId asc,index desc;$filter=totalCount eq 1)",
+                    """
+                                            [{ "_id": 1, "index": 1, "parentId": null, "categoryId": 1,
+                                            "treeType2s": [
+                                                        { "categoryId": 2, "totalCount": 1}
+                                                    ]
+                                            }]
+                         """,
+                    JSONCompareMode.NON_EXTENSIBLE),
         Arguments.of(
             TREETYPE1_MONGO_COLLECTION_USAGE_INFO,
             Set.of(10),
