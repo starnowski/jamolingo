d7b317d636cb80eb79b1ec2365d8c427ae3a24d6
starnowski <33316705+starnowski@users.noreply.github.com>
2026-09-19 22:19:50 +0200

    #54 - Added exception class [skip ci]

--- core/src/main/java/com/github/starnowski/jamolingo/core/operators/expand/MissingNavigationPropertyExpandException.java
+++ core/src/main/java/com/github/starnowski/jamolingo/core/operators/expand/MissingNavigationPropertyExpandException.java
@@ -1,16 +1,13 @@
 package com.github.starnowski.jamolingo.core.operators.expand;
 
-public class MissingNavigationPropertyExpandException extends ExpandException {
-
-  private final String missingNavigationProperty;
+public class MissingNavigationPropertyExpandException extends MissingPropertyExpandException {
 
   public MissingNavigationPropertyExpandException(
       String edmPath, String missingNavigationProperty, String message) {
-    super(edmPath, message);
-    this.missingNavigationProperty = missingNavigationProperty;
+    super(edmPath, missingNavigationProperty, message);
   }
 
   public String getMissingNavigationProperty() {
-    return missingNavigationProperty;
+    return getMissingProperty();
   }
 }
